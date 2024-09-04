e723ed19d04e574b42c948cbd3eb4b0ac5c68a54
Osttik <lololka23@gmail.com>
2024-09-04 16:27:59 +0300

    google request model

---
+++ Models/Requests/GoogleLoginModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+#nullable disable
+
+namespace Game.GamePlay.Cards.Shared.Models.Requests
+{
+    public class GoogleLoginModel
+    {
+        [Required]
+        public string Token { get; set; }
+    }
+}
